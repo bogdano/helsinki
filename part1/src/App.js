be2c3093c671b58933f16ab7eb16495acc722aad
Bogdan Boskovic <bogdano@users.noreply.github.com>
2023-02-05 02:24:22 -0700

    Update App.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,25 @@
-const Hello = () => {  
-  return (    
-    <div>
-      <p>Hello world</p>
-    </div>  
-  )
-}
-
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <p>Hello world, it is {now.toDateString()}</p>
+      <h1>{course}</h1>
       <p>
-        {a} plus {b} is {a + b}
+        {part1} {exercises1}
       </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
